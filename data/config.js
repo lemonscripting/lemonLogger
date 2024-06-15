298bc8c4c783682c2c1cf52279a1f39ec6b94acb
lemonscripting <147532946+lemonscripting@users.noreply.github.com>
2024-06-15 15:45:05 +0800

    format

--- data/config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDXvvX40sk5WOy42AvFBXD7D27Zg4NnEQI",
-    authDomain: "general-verification.firebaseapp.com",
-    projectId: "general-verification",
-    storageBucket: "general-verification.appspot.com",
-    messagingSenderId: "467046235726",
-    appId: "1:467046235726:web:9947ce789373f83af00d1f",
-    measurementId: "G-ESG3WFGYTW"
-};
